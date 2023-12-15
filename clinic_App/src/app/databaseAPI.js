fc7f9696ffe5663cefe5c89e3acf5cfff89ed9a6
Wilson Mutua <Wwmutua@healthstart.co.ke>
2023-12-15 10:44:49 +0300

    clinic APP

--- clinic_App/src/app/databaseAPI.js
+++ clinic_App/src/app/databaseAPI.js
@@ -24,15 +24,16 @@ app.use(cors());
 
 // Middleware for parsing JSON
 app.use(express.json());
+// let alternativecontactID;
 
 // API Endpoint for Form Submission
 app.post('/api/submit-form', (req, res) => {
-    var genderint = 1;
-    var alternativecontactID = 1;
-    var patientNo = 1;
+    let genderint ;
+    let patientNo;
+    let alternativecontactID;
 
     const formData = req.body;
-    // console.log(formData.firstName)
+    console.log(formData)
     const patienttable = 'INSERT INTO patient SET ?';
     const alternativecontact = 'INSERT INTO alternativecontact SET ?';
     const appointment = 'INSERT INTO appointment SET ?';
@@ -51,85 +52,90 @@ app.post('/api/submit-form', (req, res) => {
             return;
         }
 
-        res.status(201).json({ message: 'alternative Contact submitted successfully' });
-    });
-    const alternativeidsql = 'SELECT alternativeContactId FROM alternativecontact WHERE mobile = ?';
-    // query alternativecontact ID
+        const alternativeidsql = 'SELECT alternativeContactId FROM alternativecontact WHERE mobile = ?';
+        // query alternativecontact ID
 
-    connection.query(alternativeidsql,[formData.telephone], (err, results) => {
-        if (err) {
-            console.error('Error executing query: ' + err.stack);
-            res.status(500).json({ error: 'Internal Server Error SELECT alternativeContactId' });
-            return;
-        }
-        alternativecontactID = results[0].alternativeContactId;;
-    })
 
-    // query gender ID
-    const gendersql = 'SELECT genderID FROM gender WHERE genderName = ?';
-
-    connection.query(gendersql, [formData.gender] + formData.gender, (err, results) => {
-        if (err) {
-            console.error('Error executing query: ' + err.stack);
-            res.status(500).json({ error: 'Internal Server Error SELECT genderID' });
-            return;
-        }
-        genderint = results;
-    })
-
-    // console.log('hereeee: '+ alternativecontactID)
-
-    const patientdata = {
-        fname: formData.firstName,
-        lname: formData.surname,
-        othernames: formData.othernames,
-        DOB: formData.dateOfBirth,
-        ID_No: formData.idNo,
-        gender: genderint,
-        county: formData.county,
-        mobile: formData.mobile,
-        email: formData.email,
-        AlternativeContactId: alternativecontactID,
-        disability:formData.disability
-    }
-
-    // Insert the form data into the database
+        connection.query(alternativeidsql, [formData.telephone], (err, results) => {
+            if (err) {
+                console.error('Error executing query: ' + err.stack);
+                res.status(500).json({ error: 'Internal Server Error SELECT alternativeContactId' });
+                return;
+            }
+            alternativecontactID = results[0].alternativeContactId
+            // console.log('hereeeeree: ' + alternativecontactID)
 
 
 
-    connection.query(patienttable, patientdata, (err, results) => {
-        if (err) {
-            console.error('Error executing query: ' + err.stack);
-            res.status(500).json({ error: 'Internal Server Error patient Data' });
-            return;
-        }
+            // query gender ID
+            const gendersql = 'SELECT genderID FROM gender WHERE genderName = ?';
 
-        res.status(201).json({ message: 'patient Data submitted successfully' });
-    });
-    connection.query('SELECT patient_NO from patient where ID_No = ' + formData.ID, (err, results) => {
-        if (err) {
-            console.error('Error executing query: ' + err.stack);
-            res.status(500).json({ error: 'Internal Server Error SELECT patient_NO' });
-            return;
-        }
-        patientNo = results;
-    })
-    const appointmentdata = {
-        patientNo:patientNo,
-        appDate:formData.appDate,
-        appTime:formData.appTime,
+            connection.query(gendersql, [formData.gender], (err, results) => {
+                if (err) {
+                    console.error('Error executing query: ' + err.stack);
+                    res.status(500).json({ error: 'Internal Server Error SELECT genderID' });
+                    return;
+                }
+                genderint = results[0].genderID;
 
 
-    }
-// add appointment
-    connection.query(appointment, appointmentdata, (err, results) => {
-        if (err) {
-            console.error('Error executing query: ' + err.stack);
-            res.status(500).json({ error: 'Internal Server Error appointment data' });
-            return;
-        }
+                // console.log('hereeee: ' + formData)
+                // alternativecontactID = 
+                const patientdata = {
+                    fname: formData.firstName,
+                    lname: formData.surname,
+                    othernames: formData.othernames,
+                    DOB: formData.dateOfBirth,
+                    ID_No: formData.idNo,
+                    gender: genderint,
+                    county: formData.county,
+                    mobile: formData.mobile,
+                    email: formData.email,
+                    AlternativeContactId: alternativecontactID,
+                    disability: formData.hasDisability
+                }
 
-        res.status(201).json({ message: 'appointment data  submitted successfully' });
+                // Insert the form data into the database
+
+
+
+                connection.query(patienttable, patientdata, (err, results) => {
+                    if (err) {
+                        console.error('Error executing query: ' + err.stack);
+                        res.status(500).json({ error: 'Internal Server Error patient Data' });
+                        return;
+                    }
+
+
+                    connection.query('SELECT patient_NO from patient where ID_No = ' + formData.idNo, (err, results) => {
+                        if (err) {
+                            console.error('Error executing query: ' + err.stack);
+                            res.status(500).json({ error: 'Internal Server Error SELECT patient_NO' });
+                            return;
+                        }
+                        patientNo = results[0].patient_NO;
+
+                        const appointmentdata = {
+                            patientNo: patientNo,
+                            appDate: formData.appointmentDate,
+                            appTime: formData.appointmentTime,
+
+
+                        }
+                        // add appointment
+                        connection.query(appointment, appointmentdata, (err, results) => {
+                            if (err) {
+                                console.error('Error executing query: ' + err.stack);
+                                res.status(500).json({ error: 'Internal Server Error appointment data' });
+                                return;
+                            }
+
+                            res.status(201).json({ message: 'data  submitted successfully' });
+                        });
+                    });
+                });
+            });
+        });
     });
 });
 
